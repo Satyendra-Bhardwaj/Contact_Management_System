ac61bc51195708d9c20e7d3cb9e4960da375ad97
Satyendra-Bhardwaj <sk.kandheli@gmail.com>
2026-07-05 12:04:40 +0530

    Add files via upload

---
+++ Contact.java
@@ -0,0 +1,49 @@
+/*
+  A console-based Contact Management System built using Java.
+
+## Technologies
+- Java
+- OOP
+- ArrayList
+- File Handling
+- Exception Handling
+
+*/
+
+public class Contact {
+
+    private String name;
+    private String phone;
+    private String email;
+
+    public Contact(String name, String phone, String email) {
+        this.name = name;
+        this.phone = phone;
+        this.email = email;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    @Override
+    public String toString() {
+        return name + "," + phone + "," + email;
+    }
+}
